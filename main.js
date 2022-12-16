41eb77dd1e1bb9b1538b090c828fc381d00d279d
Martin Cosimano <martincosimano@hotmail.com>
2022-12-16 14:40:17 -0300

    added some js to do night mode

---
+++ main.js
@@ -0,0 +1,33 @@
+document.querySelector('#darkMode').addEventListener('click', shiftMode)
+
+function shiftMode() {
+    document.body.classList.toggle('dark-mode')
+    document.body.classList.toggle('dark-mode-fonts')
+    
+    document.querySelector('.header-text').classList.add('dark-mode-ommit')
+    document.querySelector('.main-text').classList.add('dark-mode-ommit')
+
+    const projectSectionParagraphs = document.querySelectorAll('.project-section-p')
+    for (let i = 0; i < projectSectionParagraphs.length; i++) {
+        projectSectionParagraphs[i].classList.add('dark-mode-ommit')
+    }
+
+    const serviceIcons = document.querySelectorAll('.service-icons')
+    for (let i = 0; i < serviceIcons.length; i++) {
+        serviceIcons[i].classList.add('dark-mode-ommit')
+    }
+
+    // const buttons = document.querySelectorAll('.button')
+    // for (let i = 0; i < buttons.length; i++) {
+    //     buttons[i].classList.add('dark-mode-buttons')
+    // }
+
+    const darkIcon = document.querySelector('#darkMode')
+    if(darkIcon.className === 'fa-regular fa-sun') {
+        darkIcon.className = 'fa-regular fa-moon'
+        console.log("moon")
+    }else if(darkIcon.className === 'fa-regular fa-moon'){
+        darkIcon.className = 'fa-regular fa-sun'
+        console.log("sun")
+    }
+}
