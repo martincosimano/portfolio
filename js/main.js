3005d56d7d45e0963261349d366b4418e3e4010a
Martin Cosimano <martincosimano@hotmail.com>
2022-12-17 12:39:20 -0300

    fixed night mode, now maintaining through navegating. Improved js function shiftMode

--- js/main.js
+++ js/main.js
@@ -1,37 +1,31 @@
 document.querySelector('#darkMode').addEventListener('click', shiftMode)
 
+const darkIcon = document.querySelector('#darkMode')
+
 function shiftMode() {
     document.body.classList.toggle('dark-mode')
     document.body.classList.toggle('dark-mode-fonts')
     
     document.querySelector('.header-text').classList.add('dark-mode-ommit')
 
-    const mainText = document.querySelectorAll('.main-text')
-    for (let i = 0; i < mainText.length; i++) {
-        mainText[i].classList.add('dark-mode-ommit')
-    }
+    document.querySelectorAll(".main-text").forEach(x=>x.classList.add("dark-mode-ommit"))
+    document.querySelectorAll(".button").forEach(x=>x.classList.add("dark-mode-buttons"))
+    document.querySelectorAll(".project-section-p").forEach(x=>x.classList.add("dark-mode-ommit"))
+    document.querySelectorAll(".service-icons").forEach(x=>x.classList.add("dark-mode-ommit"))
 
-    const buttons = document.querySelectorAll('.button')
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].classList.add('dark-mode-buttons')
-    }
-
-    const projectSectionParagraphs = document.querySelectorAll('.project-section-p')
-    for (let i = 0; i < projectSectionParagraphs.length; i++) {
-        projectSectionParagraphs[i].classList.add('dark-mode-ommit')
-    }
-
-    const serviceIcons = document.querySelectorAll('.service-icons')
-    for (let i = 0; i < serviceIcons.length; i++) {
-        serviceIcons[i].classList.add('dark-mode-ommit')
-    }
-
-    const darkIcon = document.querySelector('#darkMode')
-    if(darkIcon.className === 'fa-regular fa-sun') {
+    const returnIndex = document.querySelectorAll('.returnIndex')
+    const returnAbout = document.querySelectorAll('.returnAbout')
+   if(darkIcon.className === 'fa-regular fa-sun') {
         darkIcon.className = 'fa-regular fa-moon'
-        console.log("moon")
+        document.querySelectorAll(".returnIndex").forEach(x=>x.href = "index.html")
+        document.querySelectorAll(".returnAbout").forEach(x=>x.href = "about.html")
+        console.log("day")
     }else if(darkIcon.className === 'fa-regular fa-moon'){
         darkIcon.className = 'fa-regular fa-sun'
-        console.log("sun")
+        document.querySelectorAll(".returnIndex").forEach(x=>x.href = "index2.html")
+        document.querySelectorAll(".returnAbout").forEach(x=>x.href = "about2.html")
+        console.log("night") 
+
     }
+
 }
